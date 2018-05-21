844ecf3357de685ef329ff763b3f2033d85a43b8
westonkelliher <westonkelliher@ucla.edu>
2018-05-21 13:14:13 -0700

    de-hard-coded stake endpoint

--- delphi_api/app/migrations/seed.py
+++ delphi_api/app/migrations/seed.py
@@ -21,23 +21,62 @@ def seed(engine):
     # Session = sessionmaker(bind=engine)
     # session = Session()
 
-    # s = Stake(
-    #     '0x0000000000000000000000000000000000000000',
-    #     '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
-    #     100,
-    #     'I love cats',
-    #     5,
-    #     999999999        #standing in for unix time
-    # )
-    #
-    # w = Whitelistee(
-    #     s.address,
-    #     '0x0000000000000000000000000000000000000001',
-    #     999999999             #standing in for unix time
-    # )
-    #
-    # s.whitelist.append(w)
+    tok = Token(
+        '0x0000000000000000000000000000000000000333',
+        'test-token',
+        'test-symbol',
+        3.14159
+    )
+    
+    arb = Arbiter(
+        '0x0000000000000000000000000000000000000334',
+        'AVGTBAS Arbitration',
+        'A very good TCR-based arbiter set'
+    )
 
-    # session.add_all([s,w])
-    #
-    # session.commit()
+    
+    sta = Stake(
+        '0x0000000000000000000000000000000000000000',
+        '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+        100,
+        'I love cats',
+        5,
+        999999999,        #standing in for unix time
+        '0x0000000000000000000000000000000000000333',
+        '0x0000000000000000000000000000000000000334'
+
+    )
+    
+    whi = Whitelistee(
+         sta.address,
+         '0x0000000000000000000000000000000000000001',
+         999999999             #standing in for unix time
+    )
+
+    cla = Claim(
+        sta.address,
+        0,
+        'Angry guy',
+        23.5,
+        arb.address,
+        .65,
+        .05,
+        'You owe me money?',
+        0,
+        False,
+        True
+    )
+    
+    sta.whitelist.append(whi)
+    sta.claims.append(cla)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add_all([tok, arb])
+
+    session.commit()
+    
+    session.add_all([sta,whi, cla])
+    
+    session.commit()
