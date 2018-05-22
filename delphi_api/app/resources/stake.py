bc8fde0f2538a953aa686862cf9d88b306ac494d
Matt Garnett <matt@mattgarnett.com>
2018-05-22 15:16:03 -0700

    updated endpoint with new model format

--- delphi_api/app/resources/stake.py
+++ delphi_api/app/resources/stake.py
@@ -31,30 +31,30 @@ class StakeEndpoint(object):
              'settlement_failed': el.settlement_failed
             }
             for el in stake_info.claims ]
-        
+
         #foreign tables (1 to 1)
-        token_info = session.query(Token).filter_by(address=stake_info.token).first()
-        arbiter_info = session.query(Arbiter).filter_by(address=stake_info.arbiter).first()
+        # token_info = session.query(Token).filter_by(address=stake_info.token).first()
+        # arbiter_info = session.query(Arbiter).filter_by(address=stake_info.arbiter).first()
 
         #TODO: de-hard-code settlements once it is implemented for the DB
-        
-        
+
+
         resp.body = json.dumps({
             'data': {
                 'staker': stake_info.staker,
                 'value': stake_info.claimable_stake,
                 'token': {
-                    'name': token_info.name,
-                    'symbol': token_info.symbol,
-                    'address': token_info.address
+                    'name': stake_info.token.name,
+                    'symbol': stake_info.token.symbol,
+                    'address': stake_info.token.address
                 },
                 'minimum_fee': stake_info.minimum_fee,
                 'data': stake_info.data,
                 'claim_deadline': str( int(Decimal(stake_info.claim_deadline.real)) ),
                 'arbiter': {
-                    'name': arbiter_info.name,
-                    'description': arbiter_info.description,
-                    'address': arbiter_info.address
+                    'name': stake_info.arbiter.name,
+                    'description': stake_info.arbiter.description,
+                    'address': stake_info.arbiter.address
                 },
                 'whitelisted_claimants': whitelist_info,
                 'claims': claims_info,
