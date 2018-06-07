6727a2b7fb121e1f472482ab4f3bdec788d24c5d
Alex Wu <alexdwu13@gmail.com>
2018-06-07 14:41:19 -0700

    Added docstrings to endpoints

--- delphi_api/app/resources/endpoints.py
+++ delphi_api/app/resources/endpoints.py
@@ -13,10 +13,18 @@ session = Session()
 
 #TODO: refactor to_checksum_address function to elsewhere to reduce repeat code
 
-#/stake/{address}
-class StakeEndpoint(object):
-    def on_get(self, req, resp, address):
 
+class StakeEndpoint(object):
+    """
+    Endpoint that handles GET requests at: /stake/{address}
+
+    Parameters:
+        address
+
+    Returns:
+        JSON structure to client of the stake with address: address
+    """
+    def on_get(self, req, resp, address):
 
         #entry point for stake data
         stake_info = session.query(Stake).get(to_checksum_address(address))
@@ -26,35 +34,70 @@ class StakeEndpoint(object):
 
         resp.body = stake_info.toJSON()
 
-        
-#/claimant/{address}
+
 class ClaimantEndpoint(object):
+    """
+    Endpoint that handles GET requests at: /claimant/{address}, and returns
+    information about stakes the claimant is involved with.
+
+    Parameters:
+        address
+
+    Returns:
+        JSON structure containing an array of stakes with claimant = address
+    """
     def on_get(self, req, resp, address):
 
         claims_info = session.query(Claim).filter_by(claimant=to_checksum_address(address)).all()
         resp.body = json.listToJSON(claims_info, [])
 
-        
-#/staker/{address}        
+
 class StakerEndpoint(object):
+    """
+    Endpoint that handles GET requests at: /staker/{address}, and returns
+    information about stakes the staker has created.
+
+    Parameters:
+        address
+
+    Returns:
+        JSON structure containing an array of stakes with staker = address
+    """
     def on_get(self, req, resp, address):
-        
+
         stakes_info = session.query(Stake).filter_by(staker=to_checksum_address(address)).all()
         resp.body = json.listToJSON(stakes_info, ['stakes'])
 
-        
-#/whitelistee/{address}
+
 class WhitelisteeEndpoint(object):
+    """
+    Endpoint that handles GET requests at: /whitelistee/{address}, and returns
+    information about stakes the whitelistee is whitelisted on.
+
+    Parameters:
+        address
+
+    Returns:
+        JSON structure containing an array of stakes with whitelist = address
+    """
     def on_get(self, req, resp, address):
 
         whitelists_info = session.query(Stake).filter(Stake.whitelist.any(claimant=to_checksum_address(address))).all()
         resp.body = json.listToJSON(whitelists_info, ['stakes'])
-                            
 
-#/arbiter/{address}        
+
 class ArbiterEndpoint(object):
+    """
+    Endpoint that handles GET requests at: /arbiter/{address}, and returns
+    information about stakes the arbiter is arbiting on.
+
+    Parameters:
+        address
+        
+    Returns:
+        JSON structure containing an array of stakes with arbiter = address
+    """
     def on_get(self, req, resp, address):
 
         claims_info = session.query(Claim).filter_by(arbiter=to_checksum_address(address)).all()
         resp.body = json.listToJSON(claims_info, [])
-                            
